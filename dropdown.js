ed9a028bd15508dd99ab1c705265fc3b0ce9ef43
yrungr <60988124+yrungr@users.noreply.github.com>
2020-06-23 16:56:42 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,14 +1,14 @@
 
-		var optionList = [];
-		var selectElement = document.getElementById('overlay');
+    var optionList = [];
+    var selectElement = document.getElementById('overlay');
     var optionElement;
-		optionList.push(['Power - Average','powerAverage',false]);
-		optionList.push(['Power - Maximum','powerMaximum',false]);
-		optionList.push(['Slot availability','slotAvailability',true]);
-		optionList.forEach(function(option){
+    optionList.push(['Power - Average','powerAverage',false]);
+    optionList.push(['Power - Maximum','powerMaximum',false]);
+    optionList.push(['Slot availability','slotAvailability',true]);
+    optionList.forEach(function(option){
       optionElement = document.createElement('option');
       optionElement.text = option[0];
       optionElement.value = option[1];
       optionElement.disabled = option[2];
       selectElement.add(optionElement);
-		});
+    });
