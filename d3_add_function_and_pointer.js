d0bf38ec2af1f5971bf92a7daa434796672bca04
yrungr <60988124+yrungr@users.noreply.github.com>
2020-07-13 19:07:08 +0100

    Update d3_add_function_and_pointer.js

--- d3_add_function_and_pointer.js
+++ d3_add_function_and_pointer.js
@@ -2,7 +2,7 @@
           classTargets = document.getElementsByClassName(dataButtonClass);
           Array.from(classTargets).forEach(function(target){
             target.addEventListener("click", dataButton, false);
-					  target.addEventListener("mouseover", function(d) {
+            target.addEventListener("mouseover", function(d) {
               d3.select(this).style("cursor", "pointer")
             });
             target.addEventListener("mouseout", function(d) {
