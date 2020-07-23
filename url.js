0b1508b17ad323074e6ebb5b6347b77d28e90547
yrungr <60988124+yrungr@users.noreply.github.com>
2020-07-23 23:41:26 +0100

    Create url.js

---
+++ url.js
@@ -0,0 +1,29 @@
+// Prints complete URL
+document.write(window.location.href);
+  
+// Prints protocol like http: or https:
+document.write(window.location.protocol); 
+ 
+// Prints hostname with port like localhost or localhost:3000
+document.write(window.location.host);
+  
+// Prints hostname like localhost or www.example.com
+document.write(window.location.hostname);
+ 
+// Prints port number like 3000
+document.write(window.location.port);
+  
+// Prints pathname like /products/search.php
+document.write(window.location.pathname); 
+ 
+// Prints query string like ?q=ipad
+document.write(window.location.search);
+ 
+// Prints fragment identifier like #featured
+document.write(window.location.hash);
+
+
+
+
+// servicenow server side
+gs.getProperty("glide.servlet.uri")
