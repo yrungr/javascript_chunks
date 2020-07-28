7a44f3989ae08351fb6c51ff654f8bdc57ab4b6e
magnus michaelson <magnusmichaelson@protonmail.com>
2020-07-28 12:10:37 +0100

    :)

---
+++ filter_arrow_functions.js
@@ -0,0 +1,26 @@
+var input =[
+  {"name": "bob",
+   "item": "apple"
+  },
+  {"name": "bob",
+   "item": "banana"
+  },
+  {"name": "charlie",
+   "item": "carrot"}
+]
+
+/*
+// old method
+var output = [];
+input.forEach(function(thing){
+  if (thing["name"] == "bob"){
+    output.push(thing);
+  }
+})
+*/
+
+// new method
+var output = input.filter(thing => thing["name"] === "bob");
+
+console.log(input);
+console.log(output)
